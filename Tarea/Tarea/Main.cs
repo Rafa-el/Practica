384f0f49cbeebe59ce15ae24d78e7025b7e3b49e
Rafael <rafa-el>
2013-09-02 23:03:57 -0500

    Programa que solicita un nombre solo para prueba

---
+++ Tarea/Tarea/Main.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Tarea
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string var;
+			Console.Write ("Hola, ingresa un nombre: ");
+			var=Console.ReadLine();
+			Console.WriteLine("{0}, Gracias esta es una prueba de programa", var);
+
+		}
+	}
+}
